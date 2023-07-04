3fbb87a3df7d6806c21e3cf8d1e4088c8ffc49d0
EC2 Default User <ec2-user@ip-172-31-1-21.ap-northeast-1.compute.internal>
2023-07-04 06:33:15 +0000

    device退会機能実装途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  #root to:はサイトのルートページを決める記述
+  root to: "homes#top"
+  #devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # 会員側のルーティング設定
   get 'items' => 'public/items#index'
--- db/migrate/20230703111437_create_oder_items.rb
+++
@@ -1,9 +0,0 @@
-class CreateOderItems < ActiveRecord::Migration[6.1]
-  def change
-    create_table :oder_items do |t|
-      t.integer :amoumt, null: false
-      t.integer :price, null: false
-      t.timestamps
-    end
-  end
-end
