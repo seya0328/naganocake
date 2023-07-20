73ace74188a7b705e4726570145abc7dfbd0baa2
EC2 Default User <ec2-user@ip-172-31-1-21.ap-northeast-1.compute.internal>
2023-07-20 11:05:11 +0000

    商品一覧画面完了

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,13 @@
 class Public::CartItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+  def update
+  end
+  def destroy
+  end
+  def destroy_all
+  end
+  def create
+  end
 end
