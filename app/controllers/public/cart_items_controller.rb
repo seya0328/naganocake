92e81d2b4293b703a45f7f164774902da6824668
EC2 Default User <ec2-user@ip-172-31-1-21.ap-northeast-1.compute.internal>
2023-07-26 06:53:20 +0000

    カート機能一覧途中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,18 +3,33 @@ class Public::CartItemsController < ApplicationController
     @items = Item.all
     @cart_items = current_customer.cart_items
   end
+  
   def update
   end
+  
   def destroy
-    
   end
+  
   def destroy_all
     current_user.books.destroy_all
   end
+  
   def create
-    @cart_item = CartItem.new(item_params)
+    binding.pry
+    #新しくカートに入れた商品をcart_itemモデルに保存している
+    #(cart_item_params)は下で定義しているものが呼び出されている
+    @cart_item = CartItem.new(cart_item_params)
+    #カートに入れた会員とログインしたcustomer.idは等しい
     @cart_item.customer_id = current_customer.id
+    #カートに入れたアイテムを保存する
     @cart_item.save
-    
+    #createアクションが実行された時、遷移されるページをredirect_toとパスで指定する
+    redirect_to cart_items_path
   end
+  
+  private 
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
+  
 end
